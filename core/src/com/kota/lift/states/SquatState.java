b5548da6cbd322194572cc883394b835075b0a4b
Kota <p.kovacstamas@gmail.com>
2016-07-21 13:48:11 +0200

    Weight raising and decreasing logic added, along with corresponding texture selection and presentation.

--- core/src/com/kota/lift/states/SquatState.java
+++ core/src/com/kota/lift/states/SquatState.java
@@ -2,6 +2,7 @@ package com.kota.lift.states;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.kota.lift.CustomGestureDetector;
 import com.kota.lift.Lift;
@@ -12,14 +13,16 @@ import com.kota.lift.entities.Bar;
  */
 public class SquatState extends State {
     private Bar bar;
+    private Texture guard;
 
     public SquatState(GameStateManager gsm, OrthographicCamera camera) {
         super(gsm, camera);
-        bar = new Bar(388, 300);
+        bar = new Bar(382, 350);
+        guard = new Texture("guard.png");
         Gdx.input.setInputProcessor(new CustomGestureDetector(new CustomGestureDetector.DirectionListener() {
             @Override
             public void onUp() {
-
+                bar.raiseWeight();
             }
             @Override
             public void onRight() {
@@ -35,7 +38,7 @@ public class SquatState extends State {
             }
             @Override
             public void onDown() {
-
+                bar.decreaseWeight();
             }
         }));
     }
@@ -63,6 +66,20 @@ public class SquatState extends State {
         batch.begin();
         batch.draw(background, 0, 0);
         batch.draw(bar.getImage(), bar.getPosition().x, bar.getPosition().y);
+
+        int j = 0;
+        while (!(j >= bar.getWeights().length) && bar.getWeights()[j] != 0) j++;
+
+        for (int i = j - 1; i >= 0; i--){
+            //if(bar.getWeights()[i] != 0){
+                batch.draw(bar.getTextureHolder()[i], bar.getPosition().x + 55 - (3 * i), bar.getPosition().y - 1);
+                batch.draw(bar.getTextureHolder()[i], bar.getPosition().x + 186 + (3 * i), bar.getPosition().y - 1,
+                        bar.getTextureHolder()[i].getWidth(), bar.getTextureHolder()[i].getHeight(),
+                        0, 0, bar.getTextureHolder()[i].getWidth(), bar.getTextureHolder()[i].getHeight(), true, false);
+            //}
+        }
+        batch.draw(guard, bar.getPosition().x + 63, bar.getPosition().y + 12);
+        batch.draw(guard, bar.getPosition().x + 191, bar.getPosition().y + 12);
         batch.end();
     }
 
